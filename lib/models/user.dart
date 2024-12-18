00ef0ea22daa68a457ff0e92542ab2824035875e
Yosuarman <reyyosua29@gmail.com>
2024-12-19 00:41:14 +0800

    model(user): menambahkan model user

---
+++ lib/models/user.dart
@@ -0,0 +1,17 @@
+class User {
+  String? username;
+  String? email;
+  String? phoneNumber;
+  String? birth;
+  String? gender;
+  String? password;
+
+  User({
+    this.username,
+    this.email,
+    this.phoneNumber,
+    this.birth,
+    this.gender,
+    this.password,
+  });
+}
